b2ae9c265577b21fa331c9038a149ab8bee2da91
rui-exe <mrswager173@gmail.com>
2024-11-30 14:35:03 +0000

    Finished Chapter 16

--- internal/data/models.go
+++ internal/data/models.go
@@ -15,17 +15,17 @@ var (
 // Create a Models struct which wraps the MovieModel. We'll add other models to this,
 // like a UserModel and PermissionModel, as our build progresses.
 type Models struct {
-	Movies MovieModel
-	Users  UserModel
-	Tokens TokenModel
+	Movies      MovieModel
+	Permissions PermissionModel // Add a new Permissions field.
+	Tokens      TokenModel
+	Users       UserModel
 }
 
-// For ease of use, we also add a New() method which returns a Models struct containing
-// the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{DB: db},
-		Users:  UserModel{DB: db},
-		Tokens: TokenModel{DB: db},
+		Movies:      MovieModel{DB: db},
+		Permissions: PermissionModel{DB: db}, // Initialize a new PermissionModel instance.
+		Tokens:      TokenModel{DB: db},
+		Users:       UserModel{DB: db},
 	}
 }
