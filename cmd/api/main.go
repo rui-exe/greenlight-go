46988962a4d55c0f6e1638824554f40f736bf99a
rui-exe <mrswager173@gmail.com>
2024-12-02 03:00:49 +0000

    Changed SMTP Port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -79,7 +79,7 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 
 	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
-	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 587, "SMTP port")
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "70a724b75038cd", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "ff52decb88c8b5", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlightmovies.pt>", "SMTP sender")
